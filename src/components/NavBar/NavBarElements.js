301b5446ebadba7147f7377f8379d95747682a99
placiderapson <placideirandora@gmail.com>
2021-01-20 12:30:40 +0200

    Fix the Position issue

--- src/components/NavBar/NavBarElements.js
+++ src/components/NavBar/NavBarElements.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 export const Nav = styled.nav`
   background: #000;
   height: 80px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
