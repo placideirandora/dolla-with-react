57892b327260211340a9df5875852192e99c618e
placiderapson <placideirandora@gmail.com>
2021-01-22 10:24:50 +0200

    Create and Style the Footer section component

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import {
   homeObjTwo,
 } from '../components/InfoSection/data';
 import Services from '../components/Services';
+import Footer from '../components/Footer';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,6 +28,7 @@ const Home = () => {
       <InfoSection {...homeObjTwo} />
       <Services />
       <InfoSection {...homeObjThree} />
+      <Footer />
     </>
   );
 };
