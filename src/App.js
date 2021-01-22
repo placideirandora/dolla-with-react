5b54a3c601fbef8244ce14b6f9de601767904867
placiderapson <placideirandora@gmail.com>
2021-01-22 08:04:41 +0200

    Implement the Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages';
+import SignIn from './pages/signin';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SignIn} exact />
+      </Switch>
     </Router>
   );
 }
